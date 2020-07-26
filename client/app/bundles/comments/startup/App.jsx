cfee43c2b242565d713214fa3280d26613ecb3e1
Antonette Caldwell <pullmana8@gmail.com>
2020-07-26 14:20:14 -0500

    init on the app

---
+++ client/app/bundles/comments/startup/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactOnRails from 'react-on-rails';
+
+export default (_props, _railsContext) => {
+    const store = ReactOnRails.getStore('commentsStore');
+
+    return (
+        <Provider store={store}>
+        </Provider>
+    )
+}
