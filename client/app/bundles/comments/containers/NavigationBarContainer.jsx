70f286a19210ff285668d25d23d2234d5bce7925
Antonette Caldwell <pullmana8@gmail.com>
2020-07-26 14:29:10 -0500

    setting up the rest of the navbar

---
+++ client/app/bundles/comments/containers/NavigationBarContainer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import BaseComponent from 'libs/components/BaseComponent';
+import NavigationBar from '../components/NavigationBar/NavigationBar';
+
+function stateToProps(state) {
+    if (state.$$commentsStore) {
+        return {
+            pathname: state.railsContext.pathname,
+        };
+    }
+    return { };
+}
+
+class NavigationBarContainer extends BaseComponent {
+    static propTypes ={
+        pathname: PropTypes.string.isRequired,
+    };
+
+    render() {
+        const { pathname } = this.props;
+
+        return (
+            <NavigationBar {...{ pathname }} />
+        );
+    }
+}
+
+export default connect(stateToProps)(NavigationBarContainer);
---
+++ client/app/bundles/comments/startup/NavigationBarApp.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactOnRails from 'react-on-rails';
+import { Provider } from 'react-redux';
+
+import NavigationBar from '../components/NavigationBar/NavigationBar';
+import * as paths from '../constants/paths';
+
+export default (_props, railsContext) => {
+    const { pathname } = railsContext;
+    
+    let store;
+    if (pathname === paths.ROUTER_PATH) {
+        store = ReactOnRails.getStore('routerCommentsStore', false);
+    } else if (pathname === paths.NO_ROUTER_PATH) {
+        store = ReactOnRails.getStore('commentsStore', false);
+    } else {
+        return (
+            <NavigationBar {...{ pathname }} />
+        );
+    }
+
+    return (
+        <Provider store={store}>
+            <NavigationBarContainer />
+        </Provider>
+    )
+}
