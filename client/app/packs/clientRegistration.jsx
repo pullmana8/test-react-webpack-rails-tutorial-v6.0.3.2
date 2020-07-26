4e38b67302cf9e30e2be9afb1715b0434d949e2b
Antonette Caldwell <pullmana8@gmail.com>
2020-07-26 14:37:56 -0500

    Renamed the deafult hello_world file to client registration

--- client/app/packs/clientRegistration.jsx
+++ client/app/packs/clientRegistration.jsx
@@ -1,8 +1,14 @@
 import ReactOnRails from 'react-on-rails';
+import 'bootstrap-loader';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
+import App from '../bundles/comments/startup/App';
+import NavigationBarApp from '../bundles/comments/startup/NavigationBarApp';
+
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
+  App,
   HelloWorld,
+  NavigationBarApp
 });
