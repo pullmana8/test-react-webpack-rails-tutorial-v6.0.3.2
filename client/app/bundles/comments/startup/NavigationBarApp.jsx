1cfb4eec9bd98beef68f5ca3425459cdb3e0d2f1
Antonette Caldwell <pullmana8@gmail.com>
2020-07-27 09:17:45 -0500

    Added navigation bar container

--- client/app/bundles/comments/startup/NavigationBarApp.jsx
+++ client/app/bundles/comments/startup/NavigationBarApp.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactOnRails from 'react-on-rails';
 import { Provider } from 'react-redux';
-
 import NavigationBar from '../components/NavigationBar/NavigationBar';
+import NavigationBarContainer from '../containers/NavigationBarContainer';
 import * as paths from '../constants/paths';
 
 export default (_props, railsContext) => {
-    const { pathname } = railsContext;
-    
-    let store;
-    if (pathname === paths.ROUTER_PATH) {
-        store = ReactOnRails.getStore('routerCommentsStore', false);
-    } else if (pathname === paths.NO_ROUTER_PATH) {
-        store = ReactOnRails.getStore('commentsStore', false);
-    } else {
-        return (
-            <NavigationBar {...{ pathname }} />
-        );
-    }
+  const { pathname } = railsContext;
 
+  let store;
+  if (pathname === paths.ROUTER_PATH) {
+    store = ReactOnRails.getStore('routerCommentsStore', false);
+  } else if (pathname === paths.NO_ROUTER_PATH) {
+    store = ReactOnRails.getStore('commentsStore', false);
+  } else {
     return (
-        <Provider store={store}>
-            <NavigationBarContainer />
-        </Provider>
-    )
-}
+      <NavigationBar {...{ pathname }} />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <NavigationBarContainer />
+    </Provider>
+  );
+};
