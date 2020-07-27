c5a8e6d6e89ae96c5bac2cdb9f0e01bc16d66da8
Antonette Caldwell <pullmana8@gmail.com>
2020-07-27 09:14:22 -0500

    disable new-cap, per eslint

--- client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
+++ client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
@@ -1,3 +1,5 @@
+// eslint-disable new-cap
+
 import classNames from 'classnames';
 import _ from 'lodash';
 import React from 'react';
@@ -6,64 +8,65 @@ import PropTypes from 'prop-types';
 import * as paths from '../../constants/paths';
 
 const NavigationBar = (props) => {
-    const { pathname } = props;
+  const { pathname } = props;
 
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container">
-                <div className="navbar-header">
-                    <button
-                        type="button"
-                        className="navbar-toggle"
-                        data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1"
-                    >
-                        <span className="sr-only">Toggle Navigation</span>
-                        <span className="icon-bar" />
-                        <span className="icon-bar" />
-                        <span className="icon-bar" />
-                        <span className="icon-bar" />
-                    </button>
-                    <a className="navbar-brand" href="http://www.shakacode.com">ShakaCode</a>
-                </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav">
-                        <li className={classNames(
-                            { active: (pathname === paths.ROUTER_PATH || pathname === paths.REACT_ROUTER_PATH) },
-                        )}>
-                            <a href={paths.ROUTER_PATH}>React Router Demo</a>
-                        </li>
-                        <li className={classNames({ active: (pathname === paths.NO_ROUTER_PATH) })}>
-                            <a href={paths.NO_ROUTER_PATH}>React Demo</a>
-                        </li>
-                        <li className={classNames({ active: (pathname === paths.SIMPLE_REACT_PATH) })}>
-                            <a href={paths.SIMPLE_REACT_PATH}>Simple React</a>
-                        </li>
-                        <li className={classNames({ active: (pathname === paths.RAILS_PATH) })}>
-                            <a href={paths.RAILS_PATH}>Classic Rails</a>
-                        </li>
-                        <li>
-                            <a
-                                href={
-                                    'https://github.com/' +
-                                    'shakacode/react-webpack-rails-tutorial'
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            <span className="sr-only">Toggle Navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a className="navbar-brand" href="http://www.shakacode.com">ShakaCode</a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav">
+            <li className={classNames(
+              { active: (pathname === paths.ROUTER_PATH || pathname === paths.REACT_ROUTER_PATH) },
+            )}
+            >
+              <a href={paths.ROUTER_PATH}>React Router Demo</a>
+            </li>
+            <li className={classNames({ active: (pathname === paths.NO_ROUTER_PATH) })}>
+              <a href={paths.NO_ROUTER_PATH}>React Demo</a>
+            </li>
+            <li className={classNames({ active: (pathname === paths.SIMPLE_REACT_PATH) })}>
+              <a href={paths.SIMPLE_REACT_PATH}>Simple React</a>
+            </li>
+            <li className={classNames({ active: (pathname === paths.RAILS_PATH) })}>
+              <a href={paths.RAILS_PATH}>Classic Rails</a>
+            </li>
+            <li>
+              <a
+                href={
+                                    'https://github.com/'
+                                    + 'shakacode/react-webpack-rails-tutorial'
                                 }
-                            >
-                                Source
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://forum.shakacode.com/c/reactjs">Forum</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+              >
+                Source
+              </a>
+            </li>
+            <li>
+              <a href="https://forum.shakacode.com/c/reactjs">Forum</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 NavigationBar.propTypes = {
-    pathname: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired,
 };
 
 export default NavigationBar;
