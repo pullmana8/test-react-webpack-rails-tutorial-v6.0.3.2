e2d2175dd856fd0a04bae5d535c9a23f5e4ad2d5
Antonette Caldwell <pullmana8@gmail.com>
2020-07-27 09:14:40 -0500

    disable prefer-stateless-function and no-unescaped-entities

--- client/app/bundles/comments/layout/Layout.jsx
+++ client/app/bundles/comments/layout/Layout.jsx
@@ -4,36 +4,38 @@ import { NavLink as Link } from 'react-router-dom';
 
 import './Layout.scss';
 
+/* eslint-disable react/prefer-stateless-function */
 export default class Layout extends Component {
     static propTypes = {
-        children: PropTypes.object.isRequired,
+      children: PropTypes.object.isRequired,
     };
 
+    /* eslint-disable react/no-unescaped-entities */
     render() {
-        return (
-            <section>
-                <header>
-                    <ul>
-                        <li>
-                            <Link exact to="/" activeClassName="active">
-                                Comments (Root URL)
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/react-router" activeClassName="active">
-                                Test React Router ('/react-router')
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/react-router/redirect" activeClassName="active">
-                                Test Redirect
-                                (url to '/react-router/redirect' which goes to root '/')
-                            </Link>
-                        </li>
-                    </ul>
-                </header>
-                {this.props.children}
-            </section>
-        )
+      return (
+        <section>
+          <header>
+            <ul>
+              <li>
+                <Link exact to="/" activeClassName="active">
+                  Comments (Root URL)
+                </Link>
+              </li>
+              <li>
+                <Link to="/react-router" activeClassName="active">
+                  Test React Router ('/react-router')
+                </Link>
+              </li>
+              <li>
+                <Link to="/react-router/redirect" activeClassName="active">
+                  Test Redirect
+                  (url to '/react-router/redirect' which goes to root '/')
+                </Link>
+              </li>
+            </ul>
+          </header>
+          {this.props.children}
+        </section>
+      );
     }
 }
